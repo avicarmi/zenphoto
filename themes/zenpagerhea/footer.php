45a34c4b57c4482174d1f896f400093b783cd427
avicarmi <avi@carmi.us>
2021-07-08 02:33:42 -0700

    update from live site on server

--- themes/zenpagerhea/footer.php
+++ themes/zenpagerhea/footer.php
@@ -1,14 +1,6 @@
-<div class="rheacopy">
-Copyright &copy; 1974-2021 Rhea Carmi, All Rights Reserved.
-</div>
-
 <?php
 
 // force UTF-8 Ø
-printCopyrightNotice('', ' | ');
-
-//echo gettext("Zenpage theme designed by <a href=\"http://www.maltem.de\">Malte Müller</a>");
-?> 
-<?php 
-// printZenphotoLink(); 
-?>
+printCopyrightNotice('', '');
+/* echo gettext("Zenpage theme designed by <a href=\"http://www.maltem.de\">Malte Müller</a>"); */
+?>  <?php /* printZenphotoLink(); */?>
