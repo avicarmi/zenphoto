962dec0347017c139804992c57476d24e77e3e60
avicarmi <avi@carmi.us>
2023-02-22 21:43:52 -0800

    Add zenpagerhea theme at 1.5.9

---
+++ themes/zenpagerhea/sidebar.php
@@ -0,0 +1,44 @@
+<?php
+// force UTF-8 Ø
+
+if (function_exists('printCustomMenu') && getOption('zenpage_custommenu')) {
+	?>
+	<div class="menu">
+		<?php printCustomMenu('zenpage', 'list', '', "menu-active", "submenu", "menu-active", 2); ?>
+	</div>
+	<?php
+} else {
+	if (ZP_PAGES_ENABLED) { ?>
+		<div class="menu">
+			<?php printPageMenu("list", "", "menu-active", "submenu", "menu-active"); ?>
+		</div>
+	<?php }
+	
+	if (function_exists("printAlbumMenu")) { ?>
+		<div class="menu">
+		<?php printAlbumMenu("list", false, "", "menu-active", "submenu", "menu-active", ''); ?>
+		</div>
+	<?php }
+
+} // custom menu check end
+?>
+
+<?php
+if (getOption("zenpage_contactpage") && extensionEnabled('contact_form')) {
+	?>
+	<div class="menu">
+		<ul>
+			<li>
+				<?php
+				if ($_zp_gallery_page != 'contact.php') {
+					printCustomPageURL(gettext('Contact us'), 'contact', '', '');
+				} else {
+					echo gettext("Contact us");
+				}
+				?></li>
+		</ul>
+	</div>
+	<?php
+}
+?>
+<?php @call_user_func('printLanguageSelector'); ?>
