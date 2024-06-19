182e8485305379dcd5260a257f33275a2a8b01e9
Avi Carmi <avi@carmi.us>
2024-06-18 21:23:10 -0700

    Update theme_description.php

--- themes/zenpagerhea/theme_description.php
+++ themes/zenpagerhea/theme_description.php
@@ -5,7 +5,7 @@
 // Zenphoto theme definition file
 $theme_description['name'] = 'zenpagerhea';
 $theme_description['author'] = 'avicarmi';
-$theme_description['version'] = '1.6.3';
-$theme_description['date'] = "2023-02-22 22:56:29";
+$theme_description['version'] = '1.6.4';
+$theme_description['date'] = "2024-06-18 22:56:29";
 $theme_description['desc'] = "Rhea Carmi theme, based on theme Zenpage";
 ?>
